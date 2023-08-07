f01818f9734cbae485ae539bd49daf9af7e3e83c
Rajko Zagorac <rajkozgrc4@gmail.com>
2023-08-07 10:40:01 +0200

    M1_25: Automate deploy script

--- UI/src/contracts/RateX.ts
+++ UI/src/contracts/RateX.ts
@@ -4,7 +4,7 @@ import Web3 from "web3";
 import initRPCProvider from "../providers/RPCProvider";
 
 const web3: Web3 = initRPCProvider(42161);
-export const rateXAddress: string =  '0xe22C0E020C99e9aEd339618FdCEa2871d678Ef38'
+export const rateXAddress: string =  '0x4E56A6186983Bd4C9C77efD4210dD23652e0Fd51'
 
 export const RateXContract = new web3.eth.Contract(
     RateXAbi,
--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -3,6 +3,8 @@ const {loadFixture} = require("@nomicfoundation/hardhat-toolbox/network-helpers"
 const {expect} = require("chai")
 const {config} = require("../addresses.config");
 const {sendWethTokensToUser, approveToContract, sendERCTokensToUser} = require("./utils");
+const {resolve, join} = require("path");
+const fs = require("fs");
 
 const addresses = config[hre.network.config.chainId];
 
@@ -38,11 +40,58 @@ async function deployRateX() {
     return {rateX, addr1, addr2, addr3};
 }
 
-
-
 async function main() {
     const {rateX } = await deployRateX();
-    console.log(await rateX.getAddress());
+
+    const address = await rateX.getAddress();
+    console.log("RateX address:" + address);
+    saveNewAddressToFile(address);
+
+    const RateX = await hre.artifacts.readArtifact("RateX");
+    const rateXAbi = RateX.abi;
+    saveAbiToFile(rateXAbi);
 }
 
-main();
+function saveAbiToFile(abi) {
+    const content = `export const RateXAbi = ${ JSON.stringify(abi) }`;
+
+    const dirPath = resolve(__dirname, '../../UI/src/contracts');
+    const filePath = join(dirPath, 'RateXAbi.ts');
+    try {
+        fs.writeFileSync(filePath, content);
+        console.log("File written successfully");
+    } catch (error) {
+        console.error(`Error writing file: ${error}`);
+    }
+}
+
+function saveNewAddressToFile(address) {
+    const content =
+        `import {RateXAbi} from "./RateXAbi";
+import Web3 from "web3";
+
+import initRPCProvider from "../providers/RPCProvider";
+
+const web3: Web3 = initRPCProvider(42161);
+export const rateXAddress: string =  '${address}'
+
+export const RateXContract = new web3.eth.Contract(
+    RateXAbi,
+    rateXAddress
+);`;
+
+    const dirPath = resolve(__dirname, '../../UI/src/contracts');
+    const filePath = join(dirPath, 'RateX.ts');
+
+    try {
+        fs.writeFileSync(filePath, content);
+        console.log("File written successfully");
+    } catch (error) {
+        console.error(`Error writing file: ${error}`);
+    }
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
