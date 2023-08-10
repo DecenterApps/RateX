e6ba73e44f981c1ac22755699ee46a3178d7911c
Daniil Grbić <daniilgrbic@gmail.com>
2023-08-10 17:43:06 +0200

    M1_44: Reformat frontend (#1)
    Migrate to SASS, standardize variable names, reformat everything

--- UI/src/components/RoutingDiagram.tsx
+++ UI/src/components/RoutingDiagram.tsx
@@ -1,97 +1,98 @@
-import { useEffect, useState } from "react"
-import tokenList from "../constants/tokenList.json"
-import { Token } from "../constants/Interfaces"
-import { ArrowRightOutlined, ExpandAltOutlined } from "@ant-design/icons"
+import { useEffect, useState } from 'react'
+import tokenList from '../constants/tokenList.json'
+import { Token } from '../constants/Interfaces'
+import { ArrowRightOutlined, ExpandAltOutlined } from '@ant-design/icons'
+import './RoutingDiagram.scss'
 
 interface Pool {
-    poolId: string,
-    dexId: string
-    tokenOne: Token,
-    tokenTwo: Token
+  poolId: string
+  dexId: string
+  tokenFrom: Token
+  tokenTo: Token
 }
 
 interface Path {
-    percentage: number,
-    pools: Pool[]
+  percentage: number
+  pools: Pool[]
 }
 
 // mock data
 let route: Path[] = [
-    {
-        percentage: 60,
-        pools: [
-            {
-                dexId: "SushiSwapV2",
-                poolId: "0x0000000000000000000000000000000000000000",
-                tokenOne: tokenList[3],
-                tokenTwo: tokenList[4]
-            },
-            {
-                dexId: "SushiSwapV2",
-                poolId: "0x0000000000000000000000000000000000000000",
-                tokenOne: tokenList[4],
-                tokenTwo: tokenList[2]
-            },
-            {
-                dexId: "SushiSwapV2",
-                poolId: "0x0000000000000000000000000000000000000000",
-                tokenOne: tokenList[2],
-                tokenTwo: tokenList[5]
-            }
-        ]
-    },
-    {
-        percentage: 40,
-        pools: [
-            {
-                dexId: "SushiSwapV2",
-                poolId: "0x0000000000000000000000000000000000000000",
-                tokenOne: tokenList[3],
-                tokenTwo: tokenList[4]
-            },
-            {
-                dexId: "SushiSwapV2",
-                poolId: "0x0000000000000000000000000000000000000000",
-                tokenOne: tokenList[4],
-                tokenTwo: tokenList[5]
-            }
-        ]
-    }
+  {
+    percentage: 60,
+    pools: [
+      {
+        dexId: 'SushiSwapV2',
+        poolId: '0x0000000000000000000000000000000000000000',
+        tokenFrom: tokenList[3],
+        tokenTo: tokenList[4],
+      },
+      {
+        dexId: 'SushiSwapV2',
+        poolId: '0x0000000000000000000000000000000000000000',
+        tokenFrom: tokenList[4],
+        tokenTo: tokenList[2],
+      },
+      {
+        dexId: 'SushiSwapV2',
+        poolId: '0x0000000000000000000000000000000000000000',
+        tokenFrom: tokenList[2],
+        tokenTo: tokenList[5],
+      },
+    ],
+  },
+  {
+    percentage: 40,
+    pools: [
+      {
+        dexId: 'SushiSwapV2',
+        poolId: '0x0000000000000000000000000000000000000000',
+        tokenFrom: tokenList[3],
+        tokenTo: tokenList[4],
+      },
+      {
+        dexId: 'SushiSwapV2',
+        poolId: '0x0000000000000000000000000000000000000000',
+        tokenFrom: tokenList[4],
+        tokenTo: tokenList[5],
+      },
+    ],
+  },
 ]
 
-function Path({path}: {path: Path}) {
-    return (
-        <div className="routingDiagramPath">
-            {path.percentage}%
-            {path.pools.map((pool) => (
-                <>
-                    <ArrowRightOutlined className="routingDiagramPoolArrow"/>
-                    <Pool pool={pool}></Pool> 
-                </>
-            ))}
-        </div>
-    )
+function Path({ path }: { path: Path }) {
+  return (
+    <div className="routingDiagramPath">
+      {path.percentage}%
+      {path.pools.map((pool) => (
+        <>
+          <ArrowRightOutlined className="routingDiagramPoolArrow" />
+          <Pool pool={pool}></Pool>
+        </>
+      ))}
+    </div>
+  )
 }
 
-function Pool({pool}: {pool: Pool}) {
-    return (
-        <div className="routingDiagramPool">
-            <img src={pool.tokenOne.img} alt="assetOneLogo"/>
-            <img src={pool.tokenTwo.img} alt="assetTwoLogo"/>
-        </div>
-    )
+function Pool({ pool }: { pool: Pool }) {
+  return (
+    <div className="routingDiagramPool">
+      <img src={pool.tokenFrom.img} alt="assetFromLogo" />
+      <img src={pool.tokenTo.img} alt="assetToLogo" />
+    </div>
+  )
 }
 
 function RoutingDiagram() {
-    return (
-        <div className="routingDiagam">
-            <h4>Order Routing (mock)</h4>
-            <ExpandAltOutlined className="routingDiagramExpandIcon"></ExpandAltOutlined>
-            {route.map((path) => (
-                <Path path={path}></Path>
-            ))}
-        </div>
-    )
+  return (
+    <div className="routingDiagam">
+      <h4>Order Routing</h4>
+      <ExpandAltOutlined className="routingDiagramExpandIcon"></ExpandAltOutlined>
+      {route.map((path) => (
+        <Path path={path}></Path>
+      ))}
+    </div>
+  )
 }
 
 export default RoutingDiagram
