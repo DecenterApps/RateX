6af940a7594ff2abac12fa1e8c9f5795a4d3c80d
Dragan Mitrašinović <dragan.mitrasinovic19@gmail.com>
2023-08-22 16:15:30 +0200

    Update algo for pools with more than 2 tokens

--- UI/src/sdk/dexes/pools/SushiSwapV2.ts
+++ UI/src/sdk/dexes/pools/SushiSwapV2.ts
@@ -1,11 +1,10 @@
-import { Token, Pool } from '../../types'
+import { Pool, Token } from '../../types'
 
 export class SushiSwapV2Pool extends Pool {
-
   reserves: bigint[]
   static readonly fee: number = 0.003
 
-  protected constructor(poolId: string, dexId: string, tokens: Token[], reserves: bigint[]) {
+  constructor(poolId: string, dexId: string, tokens: Token[], reserves: bigint[]) {
     super(poolId, dexId, tokens)
     this.reserves = reserves
   }
@@ -13,8 +12,9 @@ export class SushiSwapV2Pool extends Pool {
   calculateExpectedOutputAmount(tokenIn: string, tokenOut: string, amountIn: bigint): bigint {
     const k = this.reserves[0] * this.reserves[1]
     const amount2 =
-      tokenIn === this.tokens[0].address ? this.reserves[1] - k / (this.reserves[0] + amountIn) : this.reserves[0] - k / (this.reserves[1] + amountIn)
+      tokenIn === this.tokens[0].address
+        ? this.reserves[1] - k / (this.reserves[0] + amountIn)
+        : this.reserves[0] - k / (this.reserves[1] + amountIn)
     return BigInt(Math.round(Number(amount2) * (1 - SushiSwapV2Pool.fee)))
   }
-  
 }
