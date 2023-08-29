a11e10809c59233b9ae180364d9b1855e79e8a37
Irina Tomic <irina.z.tomic@gmail.com>
2023-08-29 16:14:32 +0200

    Add Camelot off-chain calculations
    Co-Authored-By: Daniil Grbić <mi20042@alas.matf.bg.ac.rs>

--- UI/src/sdk/dexes/dexIdsList.ts
+++ UI/src/sdk/dexes/dexIdsList.ts
@@ -2,7 +2,8 @@ const dexIds = {
     CURVE: 'Curve',
     SUSHISWAP_V2: 'SUSHI_V2',
     UNISWAP_V3: 'UNI_V3',
-    CAMELOT_V2: 'Camelot_V2'
+    CAMELOT_V2: 'Camelot_V2',
+    TRADERJOE_V2: 'TRADERJOE_V2',
 }
 
 export default dexIds
