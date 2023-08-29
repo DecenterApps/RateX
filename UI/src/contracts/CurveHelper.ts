60c3f03afa95ab80c50a8ab67310ca409f50369f
Rajko Zagorac <rajkozgrc4@gmail.com>
2023-08-29 12:17:53 +0200

    Fix deployment issues and remove duplicate script for tenderly deployment

--- UI/src/contracts/CurveHelper.ts
+++ UI/src/contracts/CurveHelper.ts
@@ -4,7 +4,7 @@ import Web3 from "web3";
 import initRPCProvider from "../providers/RPCProvider";
 
 const web3: Web3 = initRPCProvider(42161);
-export const curveHelperAddress: string =  '0xd5DAa7CE7E3e0094a97Ec811c4fB39570aec39Cd'
+export const curveHelperAddress: string =  '0xf9075D003e0F222c545D3726E014ADF53e9DD391'
 
 export const CurveHelperContract = new web3.eth.Contract(
     CurveHelperAbi,
--- UI/src/contracts/RateX.ts
+++ UI/src/contracts/RateX.ts
@@ -4,7 +4,7 @@ import Web3 from "web3";
 import initRPCProvider from "../providers/RPCProvider";
 
 const web3: Web3 = initRPCProvider(42161);
-export const rateXAddress: string =  '0xC51b429Cf5cf242F03e07F97F129EE540FBceAbc'
+export const rateXAddress: string =  '0xd1Ac2155D1dC2eecfDF4b11e7912Ae7Fd8ddA8B8'
 
 export const RateXContract = new web3.eth.Contract(
     RateXAbi,
