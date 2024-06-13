4327979508d59483ca753b0902b8c09dc56fa9b2
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2024-06-13 16:33:10 +0200

    Update more config files

--- UI/src/constants/tokenAddressToImage.ts
+++ UI/src/constants/tokenAddressToImage.ts
@@ -1,46 +1,89 @@
+// Duplicate Addresses because of mainnet and arbitrum
+
 export const tokenAddressToImage: { [address: string]: any } = {
-  "0xaf88d065e77c8cc2239327c5edb3a432268e5831": {
-    "img": "https://cdn.moralis.io/eth/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
-    "ticker": "USDC"
+  '0xaf88d065e77c8cc2239327c5edb3a432268e5831': {
+    img: 'https://cdn.moralis.io/eth/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png',
+    ticker: 'USDC',
   },
-  "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8": {
-    "img": "https://assets.coingecko.com/coins/images/29429/large/usd-coin-usdc-logo-CB4C5B1C51-seeklogo.com.png?1678766616",
-    "ticker": "USDC.e"
+  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+    img: 'https://cdn.moralis.io/eth/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png',
+    ticker: 'USDC',
   },
-  "0xf97f4df75117a78c1a5a0dbb814af92458539fb4": {
-    "img": "https://cdn.moralis.io/eth/0x514910771af9ca656af840dff83e8264ecf986ca.png",
-    "ticker": "LINK"
+  '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8': {
+    img: 'https://assets.coingecko.com/coins/images/29429/large/usd-coin-usdc-logo-CB4C5B1C51-seeklogo.com.png?1678766616',
+    ticker: 'USDC.e',
   },
-  "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9": {
-    "img": "https://cdn.moralis.io/eth/0xdac17f958d2ee523a2206206994597c13d831ec7.png",
-    "ticker": "USDT"
+  '0xf97f4df75117a78c1a5a0dbb814af92458539fb4': {
+    img: 'https://cdn.moralis.io/eth/0x514910771af9ca656af840dff83e8264ecf986ca.png',
+    ticker: 'LINK',
   },
-  "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1": {
-    "img": "https://cdn.moralis.io/eth/0x6b175474e89094c44da98b954eedeac495271d0f.png",
-    "ticker": "DAI"
+  '0x514910771af9ca656af840dff83e8264ecf986ca': {
+    img: 'https://cdn.moralis.io/eth/0x514910771af9ca656af840dff83e8264ecf986ca.png',
+    ticker: 'LINK',
   },
-  "0x82af49447d8a07e3bd95bd0d56f35241523fbab1": {
-    "img": "https://cdn.moralis.io/eth/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png",
-    "ticker": "WETH"
+  '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9': {
+    img: 'https://cdn.moralis.io/eth/0xdac17f958d2ee523a2206206994597c13d831ec7.png',
+    ticker: 'USDT',
   },
-  "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f": {
-    "img": "https://cdn.moralis.io/eth/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png",
-    "ticker": "WBTC"
+  '0xdac17f958d2ee523a2206206994597c13d831ec7': {
+    img: 'https://cdn.moralis.io/eth/0xdac17f958d2ee523a2206206994597c13d831ec7.png',
+    ticker: 'USDT',
   },
-  "0x561877b6b3dd7651313794e5f2894b2f18be0766": {
-    "img": "https://cdn.moralis.io/eth/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png",
-    "ticker": "MATIC"
+  '0xda10009cbd5d07dd0cecc66161fc93d7c9000da1': {
+    img: 'https://cdn.moralis.io/eth/0x6b175474e89094c44da98b954eedeac495271d0f.png',
+    ticker: 'DAI',
   },
-  "0xfa7f8980b0f1e64a2062791cc3b0871572f1f7f0": {
-    "img": "https://cdn.moralis.io/eth/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984.png",
-    "ticker": "UNI"
+  '0x6b175474e89094c44da98b954eedeac495271d0f': {
+    img: 'https://cdn.moralis.io/eth/0x6b175474e89094c44da98b954eedeac495271d0f.png',
+    ticker: 'DAI',
   },
-  "0x11cdb42b0eb46d95f990bedd4695a6e3fa034978": {
-    "img": "https://cdn.moralis.io/eth/0xd533a949740bb3306d119cc777fa900ba034cd52.png",
-    "ticker": "CRV"
+  '0x82af49447d8a07e3bd95bd0d56f35241523fbab1': {
+    img: 'https://cdn.moralis.io/eth/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png',
+    ticker: 'WETH',
   },
-  "0x3082cc23568ea640225c2467653db90e9250aaa0": {
-    "img": "https://s2.coinmarketcap.com/static/img/coins/200x200/21106.png",
-    "ticker": "RDNT"
-  }
+  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': {
+    img: 'https://cdn.moralis.io/eth/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png',
+    ticker: 'WETH',
+  },
+
+  '0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f': {
+    img: 'https://cdn.moralis.io/eth/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png',
+    ticker: 'WBTC',
+  },
+  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599': {
+    img: 'https://cdn.moralis.io/eth/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png',
+    ticker: 'WBTC',
+  },
+  '0x561877b6b3dd7651313794e5f2894b2f18be0766': {
+    img: 'https://cdn.moralis.io/eth/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png',
+    ticker: 'MATIC',
+  },
+  '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0': {
+    img: 'https://cdn.moralis.io/eth/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png',
+    ticker: 'MATIC',
+  },
+  '0xfa7f8980b0f1e64a2062791cc3b0871572f1f7f0': {
+    img: 'https://cdn.moralis.io/eth/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984.png',
+    ticker: 'UNI',
+  },
+  '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984': {
+    img: 'https://cdn.moralis.io/eth/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984.png',
+    ticker: 'UNI',
+  },
+  '0x11cdb42b0eb46d95f990bedd4695a6e3fa034978': {
+    img: 'https://cdn.moralis.io/eth/0xd533a949740bb3306d119cc777fa900ba034cd52.png',
+    ticker: 'CRV',
+  },
+  '0xD533a949740bb3306d119CC777fa900bA034cd52': {
+    img: 'https://cdn.moralis.io/eth/0xd533a949740bb3306d119cc777fa900ba034cd52.png',
+    ticker: 'CRV',
+  },
+  '0x3082cc23568ea640225c2467653db90e9250aaa0': {
+    img: 'https://s2.coinmarketcap.com/static/img/coins/200x200/21106.png',
+    ticker: 'RDNT',
+  },
+  '0x137dDB47Ee24EaA998a535Ab00378d6BFa84F893': {
+    img: 'https://s2.coinmarketcap.com/static/img/coins/200x200/21106.png',
+    ticker: 'RDNT',
+  },
 }
