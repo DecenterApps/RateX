3312ffe84b0470832beb80f6c324bcc240635629
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2024-06-13 16:47:52 +0200

    Update components and RPC provider to handle mainnet

--- UI/src/components/RoutingDiagram.tsx
+++ UI/src/components/RoutingDiagram.tsx
@@ -1,19 +1,19 @@
-import { useEffect, useState, Fragment } from 'react'
+import { Fragment } from 'react'
 import { tokenAddressToImage } from '../constants/tokenAddressToImage'
 import { dexIdToUrl } from '../constants/dexIdToUrl'
 import { ArrowRightOutlined } from '@ant-design/icons'
 import './RoutingDiagram.scss'
-import {Quote, Route, SwapStep} from '../sdk/types'
+import { Quote, Route, SwapStep } from '../sdk/types'
 
-function RouteComponent({ route }: { route: Route }) {
+function RouteComponent({ route, chainId }: { route: Route; chainId: Number }) {
   return (
     <div className="routingDiagramRoute">
       <div className="percentage">{route.percentage}%</div>
-      <TokenComponent token={route.swaps[0].tokenIn}></TokenComponent>
+      <TokenComponent token={route.swaps[0]?.tokenIn} chainId={chainId}></TokenComponent>
       {route.swaps.map((swap, index) => (
         <Fragment key={index}>
           <TokenArrow swap={swap}></TokenArrow>
-          <TokenComponent token={swap.tokenOut}></TokenComponent>
+          <TokenComponent token={swap.tokenOut} chainId={chainId}></TokenComponent>
         </Fragment>
       ))}
     </div>
@@ -33,7 +33,7 @@ function TokenArrow({ swap }: { swap: SwapStep }) {
   )
 }
 
-function TokenComponent({ token }: { token: string }) {
+function TokenComponent({ token, chainId }: { token: string; chainId: Number }) {
   let img = 'empty-token.webp'
   let ticker = `${token.substring(0, 10)}...`
   if (token in tokenAddressToImage) {
@@ -42,24 +42,30 @@ function TokenComponent({ token }: { token: string }) {
   }
   return (
     <div className="routingDiagramToken tooltip">
-      <a href={`https://arbiscan.io/token/${token}`} target="_blank">
-        <img src={img} alt="assetFromLogo" />
-      </a>
+      {chainId === 1 ? (
+        <a href={`https://etherscan.io/token/${token}`} target="_blank">
+          <img src={img} alt="assetFromLogo" />
+        </a>
+      ) : (
+        <a href={`https://arbiscan.io/token/${token}`} target="_blank">
+          <img src={img} alt="assetFromLogo" />
+        </a>
+      )}
       <span className="tooltiptext">{ticker}</span>
     </div>
   )
 }
 
-function RoutingDiagram({ quote }: { quote: Quote | undefined }) {
+function RoutingDiagram({ quote, chainId }: { quote: Quote | undefined; chainId: Number }) {
   if (!quote || quote.quote <= 0) {
-    return (<></>)
+    return <></>
   }
   quote.routes.sort((a, b) => b.percentage - a.percentage)
   return (
     <div className="routingDiagram">
       <h4>Order Routing</h4>
       {quote.routes.map((route, index) => (
-        <RouteComponent key={-index} route={route}></RouteComponent>
+        <RouteComponent key={-index} route={route} chainId={chainId} />
       ))}
     </div>
   )
