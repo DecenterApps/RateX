3e6486901e1cb751332525e64ae912b74ca1be56
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2024-06-13 16:50:54 +0200

    Update pools and tests to handle new changes

--- UI/src/sdk/dexes/pools/uniswap/uniswapState.ts
+++ UI/src/sdk/dexes/pools/uniswap/uniswapState.ts
@@ -1,43 +1,47 @@
-import {PoolData, PoolState} from './types'
-import { UniswapHelperContract } from '../../../../contracts/rateX/UniswapHelper'
-import {convertInitialPoolDataToPoolState, convertRowPoolData} from './utils'
+import { PoolData, PoolState } from './types'
+import { convertInitialPoolDataToPoolState, convertRowPoolData } from './utils'
 import { UniswapOffchainQuoter } from './uniswapOffchainQuoter'
+import { CreateUniswapHelperContract } from '../../../../contracts/rateX/UniswapHelper'
 
 export class UniswapState {
   private static poolStateMap: Map<string, PoolState> = new Map<string, PoolState>()
   public static quoter: UniswapOffchainQuoter = new UniswapOffchainQuoter()
-  private static batch_size = 3;
+  private static batch_size = 3
 
   private constructor() {}
 
   public static getPoolState(poolAddress: string): PoolState | undefined {
-    return this.poolStateMap.get(poolAddress.toLowerCase());
+    return this.poolStateMap.get(poolAddress.toLowerCase())
   }
 
-  private static async getPoolsDataFromContract(pools: string[]): Promise<PoolData[]> {
+  private static async getPoolsDataFromContract(pools: string[], chainId: number): Promise<PoolData[]> {
     //@ts-ignore
-    const rawPoolsData: any[] = await UniswapHelperContract.methods.fetchData(pools, 15).call()
-    return rawPoolsData.map((rawPoolData: any) => convertRowPoolData(rawPoolData))
+    try {
+      const UniswapHelperContract = CreateUniswapHelperContract(chainId)
+      const rawPoolsData: any[] = await UniswapHelperContract.methods.fetchData(pools, 15).call()
+      return rawPoolsData.map((rawPoolData: any) => convertRowPoolData(rawPoolData))
+    } catch (err) {
+      console.log('Error while fetching additional data from the smart contracts: ', err)
+      throw err
+    }
   }
 
+  public static async initializeFreshPoolsData(pools: string[], chainId: number) {
+    const poolsSize = pools.length
+    const numberOfBatches = Math.ceil(poolsSize / this.batch_size)
 
-  public static async initializeFreshPoolsData(pools: string[]) {
-    const poolsSize = pools.length;
-    const numberOfBatches = Math.ceil(poolsSize / this.batch_size);
-
-    const promises: Promise<PoolData[]>[] = [];
+    const promises: Promise<PoolData[]>[] = []
 
     for (let i = 0; i < numberOfBatches; i++) {
-      const batch = pools.slice(i * this.batch_size, (i + 1) * this.batch_size);
-      promises.push(this.getPoolsDataFromContract(batch));
+      const batch = pools.slice(i * this.batch_size, (i + 1) * this.batch_size)
+      promises.push(this.getPoolsDataFromContract(batch, chainId))
     }
-
-    const allPoolsData = await Promise.all(promises);
+    const allPoolsData = await Promise.all(promises)
 
     allPoolsData.flat().forEach((poolData: PoolData) => {
-      let poolState = convertInitialPoolDataToPoolState(poolData);
+      let poolState = convertInitialPoolDataToPoolState(poolData)
       // we will store keys as lowercase addresses
-      this.poolStateMap.set(poolData.info.pool.toLowerCase(), poolState);
-    });
+      this.poolStateMap.set(poolData.info.pool.toLowerCase(), poolState)
+    })
   }
 }
