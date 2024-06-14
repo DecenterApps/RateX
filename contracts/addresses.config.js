ffa9a6830890e5ddb3dc079d9231f44f2e9bc0c2
Skocimis <aki.spremo@gmail.com>
2024-06-14 13:33:12 +0200

    added curve factory registry

--- contracts/addresses.config.js
+++ contracts/addresses.config.js
@@ -29,6 +29,7 @@ const ARBITRUM_ADDRESSES = {
         curve2Pool: '0x7f90122BF0700F9E7e1F688fe926940E8839F353', // USDCE USDT pool,
 
         poolRegistry: '0x445FE580eF8d70FF569aB36e80c647af338db351',
+        poolRegistryFactory: '0x9AF14D26075f142eb3F292D5065EB3faa646167b',
         stableSwapFactory: '0xb17b674D9c5CB2e441F8e196a2f048A81355d031',
     },
     
