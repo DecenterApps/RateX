87d7c7946fc124eba19266ccead19adf22653eeb
ViktorVujke <viktor@decenter.com>
2024-07-09 16:15:59 +0200

    New loader UI, route fetching process shown step by step in loader, integrated global state context

--- UI/src/sdk/dexes/dexIdsList.ts
+++ UI/src/sdk/dexes/dexIdsList.ts
@@ -4,6 +4,7 @@ export const dexIds = {
   UNI_V3: 'UNI_V3',
   CAMELOT: 'CAMELOT',
   BALANCER_V2: 'BALANCER_V2',
+  UNI_V2: 'UNI_V2'
 }
 
 export const balancerWeightedPoolTypes = ['Weighted', 'Managed', 'LiquidityBootstrapping']
---
+++ contracts/contracts/uniswapV2/UniswapV2Library.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import './interfaces/IUniswapV2Pair.sol';
+
+library UniswapV2Library {
+  // returns sorted token addresses, used to handle return values from pairs sorted in this order
+  function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+    require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
+    (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+    require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
+  }
+
+  // fetches and sorts the reserves for a pair
+  function getReserves(address pair, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
+    (address token0, ) = sortTokens(tokenA, tokenB);
+    (uint reserve0, uint reserve1, ) = IUniswapV2Pair(pair).getReserves();
+    (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+  }
+}
