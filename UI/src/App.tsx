a0c0cba489fbf50fee548b68dc5432fccf4511a2
ViktorVujke <158465405+ViktorVujke@users.noreply.github.com>
2024-07-19 15:30:04 +0200

    Rainbow kit chainId fix (#50)
    * rainbow wallet added
    
    * adresses in gitignore
    
    * ranbow fix arbitrum
    
    * rainbw fix

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import {
   getDefaultConfig,
   RainbowKitProvider,
+  darkTheme
 } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import {
@@ -18,27 +19,49 @@ import {
   QueryClientProvider,
   QueryClient,
 } from "@tanstack/react-query";
+
 import './App.scss'
 import 'notyf/notyf.min.css'
 import '@rainbow-me/rainbowkit/styles.css';
+import { getChainId } from '@wagmi/core'
+import { useEffect } from 'react';
 
 const config = getDefaultConfig({
   appName: 'My RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [mainnet, arbitrum],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+  ssr: false, // If your dApp uses server side rendering (SSR)
 });
 const queryClient = new QueryClient();
 
 function App() {
-  const [chainId, setChainId] = useState<number>(1)
+  const [chainId, setChainId] = useState<number>(getChainId(config))
   const [wallet, setWallet] = useState('0x0000000000000000000000000000000000000000')
+
+
+  const fetchChainId = async () => {
+    const chainIdNew = await getChainId(config);
+    if(chainIdNew != chainId)
+    setChainId(chainIdNew);
+  };
+
+  useEffect(() => {
+    // Poll the chain ID every 5 seconds
+    fetchChainId(); // Fetch initially
+    const interval = setInterval(fetchChainId, 3000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
+        <RainbowKitProvider  theme={darkTheme()} >
     <div className="App">
+      <a>{getChainId(config)}</a>
+      <a>,{chainId}</a>
+
       <Header chainIdState={[chainId, setChainId]} walletState={[wallet, setWallet]} />
       <div className="mainWindow">
         <Routes>
