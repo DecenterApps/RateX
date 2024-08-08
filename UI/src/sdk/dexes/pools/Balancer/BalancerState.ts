37cf6b01e970a199f6c7ceff4733bd870ba69643
Skocimis <aki.spremo@gmail.com>
2024-08-08 21:13:20 +0200

    balancer pool fetch parallelization

--- UI/src/sdk/dexes/pools/Balancer/BalancerState.ts
+++ UI/src/sdk/dexes/pools/Balancer/BalancerState.ts
@@ -1,34 +1,33 @@
-import { Pool, PoolInfo } from '../../../types'
-import { BalancerWeightedPool } from './BalancerWeightedPool'
-import { CreateBalancerHelperContract } from '../../../contracts/rateX/BalancerHelper'
-import Web3 from 'web3'
+import { Pool, PoolInfo } from '../../../types';
+import { BalancerWeightedPool } from './BalancerWeightedPool';
+import { CreateBalancerHelperContract } from '../../../contracts/rateX/BalancerHelper';
+import Web3 from 'web3';
 
 export class BalancerState {
     private constructor() {}
 
     public static async getPoolDataFromContract(pools: PoolInfo[], chainId: number, rpcProvider: Web3): Promise<Pool[]> {
-        let newPools: Pool[] = []
-
-        // iterate trough pools
-        for (const pool of pools) {
-            // const [balancerVault, , ] = await loadFixture(await deployBalancerDex())
-
+        const promises = pools.map(async (pool) => {
             // return token address after '-' split
-            pool.tokens.forEach((token) => token._address = token._address.split('-')[1])
+            pool.tokens.forEach((token) => token._address = token._address.split('-')[1]);
 
-            const BalancerHelperContract = CreateBalancerHelperContract(chainId, rpcProvider)
-            // @ts-ignore
-            const res: any[] = await BalancerHelperContract.methods // @ts-ignore
-                .getWeightedPoolInfo(pool.poolId)
-                .call()
-                .catch((err: any) => {
-                    console.log('Weighted Get Pool Info Error: ', err)
-                })
-            const [decimals, invariant, tokens, balances, weights, swapFeePercentage] = [res[0], res[1], res[2], res[3], res[4], res[5]]
+            const BalancerHelperContract = CreateBalancerHelperContract(chainId, rpcProvider);
+            try {
+                // @ts-ignore
+                const res: any[] = await BalancerHelperContract.methods
+                    .getWeightedPoolInfo(pool.poolId)
+                    .call();
+                
+                const [decimals, invariant, tokens, balances, weights, swapFeePercentage] = [res[0], res[1], res[2], res[3], res[4], res[5]];
+                const weightedPool = new BalancerWeightedPool(pool.poolId, pool.dexId, pool.tokens, balances, weights, swapFeePercentage);
+                return weightedPool;
+            } catch (err) {
+                console.log('Weighted Get Pool Info Error: ', err);
+                return null; // Handle the error as needed
+            }
+        });
 
-            const weightedPool = new BalancerWeightedPool(pool.poolId, pool.dexId, pool.tokens, balances, weights, swapFeePercentage)
-            newPools.push(weightedPool)
-        }
-        return newPools
+        const newPools = await Promise.all(promises);
+        return newPools.filter((pool) => pool !== null); // Filter out any null values
     }
 }
