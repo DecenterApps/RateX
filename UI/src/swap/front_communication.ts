ecedec8460a36a1ec7b9f0b5d44d14f2d62cdaf8
Aleksa Uroseivc <108018717+Scannty@users.noreply.github.com>
2024-08-14 13:59:22 +0200

    Fix rpc url forwarding (#60)

--- UI/src/swap/front_communication.ts
+++ UI/src/swap/front_communication.ts
@@ -3,13 +3,13 @@ import { executeSwap } from './solidity_communication'
 import { RateX, Dexes } from 'ratex-sdk'
 
 async function findQuote(tokenIn: string, tokenOut: string, amountIn: bigint, chainId: number): Promise<Quote> {
-  const rpcUrl = process.env.REACT_APP_MAINNET_URL || ''
+  const rpcUrl = (chainId === 1 ? process.env.REACT_APP_MAINNET_URL : process.env.REACT_APP_ARBITRUM_URL) || ''
   const graphApiKey = process.env.REACT_APP_GRAPH_API_KEY || ''
-  const dexes = [Dexes.SUSHISWAP_V2, Dexes.UNISWAP_V2, Dexes.UNISWAP_V3]
+  const dexes = [Dexes.CAMELOT]
 
   const rateX = new RateX({ rpcUrl, chainId, dexes, graphApiKey })
   const res = await rateX.getQuote(tokenIn, tokenOut, amountIn)
-  console.log(res)
+
   return res
 }
 
