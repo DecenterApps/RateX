943edc4acd518a58fd2a35221c991778b57eb06b
vaniiiii <ivandzambas@gmail.com>
2024-08-15 13:28:44 +0200

    Added NatSpec for helper contracts

--- contracts/contracts/balancer/BalancerHelper.sol
+++ contracts/contracts/balancer/BalancerHelper.sol
@@ -5,6 +5,8 @@ import {IWeightedPool} from './interfaces/IWeightedPool.sol';
 import {IStablePool} from './interfaces/IStablePool.sol';
 import {IVault} from './interfaces/IVault.sol';
 
+/// @title BalancerHelper - A helper contract for fetching Balancer pools data
+/// @notice This contract provides utility functions to retrieve information from Balancer pools
 contract BalancerHelper {
   IVault private immutable balancerVault;
 
@@ -12,18 +14,19 @@ contract BalancerHelper {
     balancerVault = IVault(_balancerVault);
   }
 
+  /// @notice Retrieves data about a weighted pool
+  /// @param _poolId The unique identifier of the Balancer pool
+  /// @return decimals The number of decimals used by the pool
+  /// @return tokens An array of token addresses in the pool
+  /// @return balances An array of token balances corresponding to the tokens in the pool
+  /// @return weights An array of normalized weights for each token in the pool
+  /// @return feePercentage The swap fee percentage applied by the pool
   function getWeightedPoolInfo(
     bytes32 _poolId
   )
     external
     view
-    returns (
-      uint8 decimals,
-      address[] memory tokens,
-      uint256[] memory balances,
-      uint256[] memory weights,
-      uint256 feePercentage
-    )
+    returns (uint8 decimals, address[] memory tokens, uint256[] memory balances, uint256[] memory weights, uint256 feePercentage)
   {
     IWeightedPool pool = IWeightedPool(getPool(_poolId));
     decimals = pool.decimals();
--- contracts/contracts/uniswapV2/UniswapV2Helper.sol
+++ contracts/contracts/uniswapV2/UniswapV2Helper.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.24;
 import {IHelperState} from '../rateX/interfaces/IHelperState.sol';
 import {UniswapV2Library} from './UniswapV2Library.sol';
 
+/// @title UniswapV2Helper - A helper contract for fetching Uniswap V2 pools data
+/// @notice This contract provides utility functions to retrieve information from Uniswap V2 pools
 contract UniswapV2Helper is IHelperState {
   struct UniswapV2Pool {
     address poolId;
@@ -11,7 +13,10 @@ contract UniswapV2Helper is IHelperState {
     Token[] tokens;
     uint[] reserves;
   }
-
+  
+  /// @notice Retrieves data for multiple Uniswap V2 pools
+  /// @param poolsInfo An array of PoolInfo containing information about each pool
+  /// @return pools An array of UniswapV2Pool structures containing the data for each pool
   function getPoolsData(PoolInfo[] memory poolsInfo) external view returns (UniswapV2Pool[] memory pools) {
     pools = new UniswapV2Pool[](poolsInfo.length);
     for (uint256 i = 0; i < poolsInfo.length; i++) {
