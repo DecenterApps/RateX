a2f76069c649d1783acf2941cb46a1eb9db16d61
Aleksa Uroseivc <108018717+Scannty@users.noreply.github.com>
2024-08-15 16:49:11 +0200

    Fix large amount input error (#62)
    * Fix large amount input error
    
    * Remove unnecessary package from package.json

--- UI/src/contracts/rateX/RateX.ts
+++ UI/src/contracts/rateX/RateX.ts
@@ -4,8 +4,8 @@ import Web3 from 'web3'
 import initRPCProvider from '../../providers/RPCProvider'
 
 export function CreateRateXContract(chainId: number) {
-    const web3: Web3 = initRPCProvider()
-    const RATE_X_ADDRESS = chainId === 1 ? '0xf682AB178cC65f48E68DfdfD376E9D1f055B8Cd4' : '0x10400dd38061186DEa9E5b2683d527A48E153ca2'
+  const web3: Web3 = initRPCProvider()
+  const RATE_X_ADDRESS = chainId === 1 ? '0xb9E24094d0899Ca4BF6FC210bD4Ee7DE7bee81CB' : '0x9DdA81Aa5FeB4a4972b62265C43dee305B360372'
 
-    return new web3.eth.Contract(RateXAbi, RATE_X_ADDRESS)
+  return new web3.eth.Contract(RateXAbi, RATE_X_ADDRESS)
 }
