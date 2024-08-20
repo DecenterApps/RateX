3f5ad62612e47aa3c7786499d8baec7c4a08b6f1
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2024-08-20 15:19:40 +0200

    Remove curve contracts from the repository

--- contracts/contracts/curve/interfaces/ICurvePoolRegistry.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
-
-interface ICurvePoolRegistry {
-
-    function get_coins(address _poolAddress) external view returns(address[8] memory);
-}
